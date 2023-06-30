78c9c156705d2d134df82bb0fddfca89472b05e8
Harry-Zapata <newdescargas21@gmail.com>
2023-06-30 13:51:43 -0500

    subida de imagenes en el cliente

--- src/controllers/principalControllers.js
+++ src/controllers/principalControllers.js
@@ -1,17 +1,7 @@
 const principalSchema = require("../models/principal");
-const fs = require('fs-extra')
-const { uploadImage, deleteImage } = require('../utils/cloudinary.js')
 
 const createPrincipal = async (req, res) => {
 	const principal = principalSchema(req.body);
-	if (req.files?.image) {
-		const result = await uploadImage(req.files.image.tempFilePath)
-		principal.image = {
-			public_id: result.public_id,
-			secure_url: result.secure_url
-		}
-		await fs.unlink(req.files.image.tempFilePath)
-	}
 	principal
 		.save()
 		.then((data) => res.json(data))
@@ -39,16 +29,6 @@ const editPrincipal = async (req, res) => {
 		const principal = await principalSchema.findById(id)
 		if (!principal)
 			return res.status(404).json({ message: "Product Not Found" });
-		if (req.files?.image) {
-			if (principal.image && principal.image.public_id) {
-				await deleteImage(principal.image.public_id);
-			}
-			const result = await uploadImage(req.files.image.tempFilePath)
-			req.body.image = {
-				public_id: result.public_id,
-				secure_url: result.secure_url
-			}
-		}
 		const updatePrincipal = await principalSchema.findByIdAndUpdate(id, req.body, {
 			new: true,
 		});
@@ -62,12 +42,6 @@ const editPrincipal = async (req, res) => {
 const deletePrincipal = async (req, res) => {
 	const { id } = req.params;
 	const principal = await principalSchema.findById(id)
-	//await deleteImage(principalSchema.image.public_id)
-	if (principal.image && principal.image.public_id) {
-		await deleteImage(principal.image.public_id);
-	}
-
-
 	principalSchema
 		.deleteOne({ _id: id })
 		.then((data) => res.json(data))
